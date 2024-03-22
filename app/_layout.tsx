0b5a6f69cb43b5eb2c99e6e8ef40f06943bd3548
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 11:30:18 +0300

    Added tanstack query to the project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import React from 'react'
 
+const queryClient = new QueryClient()
+
 const MainApplicationLayout = () => {
 	return (
-		<Stack>
-			<Stack.Screen name='index' options={{ title: 'Welcome' }} />
-			<Stack.Screen name='details' options={{ title: 'Details' }} />
-		</Stack>
+		<QueryClientProvider client={queryClient}>
+			<Stack>
+				<Stack.Screen name='index' options={{ title: 'Welcome' }} />
+				<Stack.Screen name='details' options={{ title: 'Details' }} />
+			</Stack>
+		</QueryClientProvider>
 	)
 }
 
