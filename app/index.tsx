90a2596b31cb9dac67a7649e383eaec8cff8c223
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 14:24:23 +0300

    Using a scroll view instead of a flatlist to render the cards on the index page of the application

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,32 @@
 import { dummyData } from '@/assets/dummyData'
 import Item from '@/components/item'
 import React from 'react'
-import { FlatList, StyleSheet } from 'react-native'
-import Animated, { useAnimatedRef } from 'react-native-reanimated'
+import { FlatList, ScrollView, StyleSheet } from 'react-native'
+import Animated, {
+	useAnimatedRef,
+	useScrollViewOffset
+} from 'react-native-reanimated'
 
 const ApplicationIndexPage = () => {
-	const flatListRef = useAnimatedRef<FlatList>()
+	const scrollViewRef = useAnimatedRef<ScrollView>()
+
+	// const scrollHandler = useScrollViewOffset(flatListRef)
 
 	return (
-		<Animated.FlatList
-			data={dummyData}
-			keyExtractor={(item) => item.id.toString()}
-			contentContainerStyle={styles.contentContainer}
-			showsVerticalScrollIndicator={false}
-			renderItem={({ item }) => <Item item={item} />}
-			ref={flatListRef}
-		/>
+		// <Animated.FlatList
+		// 	data={dummyData}
+		// 	keyExtractor={(item) => item.id.toString()}
+		// 	contentContainerStyle={styles.contentContainer}
+		// 	showsVerticalScrollIndicator={false}
+		// 	renderItem={({ item }) => <Item item={item} />}
+		// 	ref={flatListRef}
+		// 	onScroll={() => {}}
+		// />
+		<ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false}>
+			{dummyData.map((item, index) => (
+				<Item key={index} item={item} />
+			))}
+		</ScrollView>
 	)
 }
 
