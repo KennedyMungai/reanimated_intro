312852218296b1dd2c5a7343f65d1025da8e8c07
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 10:02:12 +0300

    Added the two pages to main layout file of the app?

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,12 @@ import { Stack } from 'expo-router'
 import React from 'react'
 
 const MainApplicationLayout = () => {
-	return <Stack></Stack>
+	return (
+		<Stack>
+			<Stack.Screen name='index' options={{ title: 'Welcome' }} />
+			<Stack.Screen name='details' options={{ title: 'Details' }} />
+		</Stack>
+	)
 }
 
 export default MainApplicationLayout
