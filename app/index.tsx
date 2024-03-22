f4c55b4a8e68d7269ca1d3200e36e3876d8c98f0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 11:07:45 +0300

    Added the animatedStyles for the background color of the TextInput component

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Button, StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, TextInput, View } from 'react-native'
 import Animated, {
 	useAnimatedStyle,
 	useSharedValue,
 	withSpring
 } from 'react-native-reanimated'
 
+const AnimatedInput = Animated.createAnimatedComponent(TextInput)
+
 const ApplicationIndexPage = () => {
 	const width = useSharedValue(200)
 	const height = useSharedValue(200)
+	const backgroundColor = useSharedValue('teal')
 
 	const startAnimation = () => {
 		const randomWidth = Math.floor(Math.random() * 300) + 100
@@ -25,9 +28,16 @@ const ApplicationIndexPage = () => {
 		}
 	})
 
+	const animatedInputStyles = useAnimatedStyle(() => {
+		return {
+			backgroundColor: backgroundColor.value
+		}
+	})
+
 	return (
 		<View style={styles.container}>
 			<Button onPress={startAnimation} title='Start Animation' />
+			<AnimatedInput style={[animatedInputStyles]} />
 			<Animated.View style={animatedStyles}></Animated.View>
 		</View>
 	)
