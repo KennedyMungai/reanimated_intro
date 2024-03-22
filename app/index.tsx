9f4b55eb3b9580b70e1d058bb20621a27471c7f1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 12:42:58 +0300

    Cleared out a lot of the logic

--- app/index.tsx
+++ app/index.tsx
@@ -2,44 +2,11 @@ import { dummyData } from '@/assets/dummyData'
 import Item from '@/components/item'
 import React from 'react'
 import { FlatList, StyleSheet, TextInput } from 'react-native'
-import Animated, {
-	useAnimatedStyle,
-	useSharedValue,
-	withSpring,
-	withTiming
-} from 'react-native-reanimated'
+import Animated from 'react-native-reanimated'
 
 const AnimatedInput = Animated.createAnimatedComponent(TextInput)
 
 const ApplicationIndexPage = () => {
-	const width = useSharedValue(200)
-	const height = useSharedValue(200)
-	const backgroundColor = useSharedValue('teal')
-
-	const startAnimation = () => {
-		const randomWidth = Math.floor(Math.random() * 300) + 100
-		const randomHeight = Math.floor(Math.random() * 300) + 100
-		const randomColor =
-			'#' + Math.floor(Math.random() * 16777215).toString(16)
-
-		width.value = withSpring(randomWidth)
-		height.value = withSpring(randomHeight)
-		backgroundColor.value = withTiming(randomColor, { duration: 1000 })
-	}
-
-	const animatedStyles = useAnimatedStyle(() => {
-		return {
-			width: width.value,
-			height: height.value
-		}
-	})
-
-	const animatedInputStyles = useAnimatedStyle(() => {
-		return {
-			backgroundColor: backgroundColor.value
-		}
-	})
-
 	return (
 		<FlatList
 			data={dummyData}
