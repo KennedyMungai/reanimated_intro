753ca0b7bca7057c663dea68277661bf016e8352
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 10:00:57 +0300

    Created two new compoonents

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ApplicationIndexPage = () => {
+	return (
+		<View>
+			<Text>ApplicationIndexPage</Text>
+		</View>
+	)
+}
+
+export default ApplicationIndexPage
+
+const styles = StyleSheet.create({})
