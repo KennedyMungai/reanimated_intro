3de0b0b85575470196adf5e2c6a6c7b8d0b760c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 12:11:23 +0300

    Removed all the tanstack query functions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import React from 'react'
 
-const queryClient = new QueryClient()
-
 const MainApplicationLayout = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Stack>
-				<Stack.Screen name='index' options={{ title: 'Welcome' }} />
-				<Stack.Screen name='details' options={{ title: 'Details' }} />
-			</Stack>
-		</QueryClientProvider>
+		<Stack>
+			<Stack.Screen name='index' options={{ title: 'Welcome' }} />
+			<Stack.Screen name='details' options={{ title: 'Details' }} />
+		</Stack>
 	)
 }
 
